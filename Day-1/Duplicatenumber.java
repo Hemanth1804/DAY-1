f5616b856a0826243c5dbc9f8841025d9c1c40c0
Hemanth1804 <147804685+Hemanth1804@users.noreply.github.com>
2025-02-07 22:13:36 +0530

    Update Duplicatenumber.java

--- Day-1/Duplicatenumber.java
+++ Day-1/Duplicatenumber.java
@@ -4,22 +4,35 @@ public class Duplicatenumber {
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the size of an Array");
     int n=sc.nextInt();
-    float[] a=new float[1000];
-    float[] b=new float[1000];
+    int[] a=new int[1000];
     int i,j;
+    int temp;
+    int flag=0;
     System.out.println("Enter"+ n +"Elements of an array");
     for(i=0;i<n;i++){
-        a[i]=sc.nextFloat();
+        a[i]=sc.nextInt();
     }
-     int flag=0;
      for(i=0;i<n;i++){
-        flag=0;
-        for(j=0;j<n;j++){
-            if(a[i]==a[j]){
-                
-            }
+        for(j=0;j<n-1-i;j++){
+             if(a[j]>a[j+1]){
+                temp=a[j+1];
+                a[j+1]=a[j];
+                a[j]=temp;
+             }
         }
      }
-
+     i=0;
+     for(;i<n;i++){
+        flag=0;
+       for(j=0;j<n;j++){
+        if(a[i]==a[j]){
+            flag+=1;
+            i=j;
+        }
+       }
+       if(flag>1){
+        System.out.println("The duplicate number is:"+a[i]+" repeadted at "+flag+" times");
+       }
+     }
     }
 }
